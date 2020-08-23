459ee8471d197617498a422bd85cb155bf1479eb
rw576 <reneise.white@gmail.com>
2020-08-23 00:39:16 -0500

    updated main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,28 +2,29 @@
 import os
 import csv
 
+#Setting Variables
+Votes = []
+County = []
+Candidates = []
+Khan = []
+Correy = []
+Li = []
+OTooley = []
+
 #connect directory
 csvpath = os.path.join('Resources', 'election_data.csv')
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     print(csvreader)
     csv_header = next(csvreader)
-    #for row in csvreader:
 
-#set variables
+#setting the loop
     for row in csvreader:
-        Votes = []
-        County = []
-        Candidates = []
-        Khan = []
-        Correy = []
-        Li = []
-        OTooley = []
-
+    
 #Setting Votes, Candidates, and County
-Votes.append(int(row[0]))
-County.append(row[1])
-Candidates.append(row[2])
+        Votes.append(int(row[0]))
+        County.append(row[1])
+        Candidates.append(row[2])
 
 #Calculating Total Votes
 Total_Votes = (len(Votes))
@@ -47,17 +48,17 @@ print(Khan_Votes)
 print(Correy_Votes)
 print(Li_Votes)
 print(OTooley_Votes)
-    
+
 #Calculating The Percentage of Votes Each Candidate Won
 Khan_Percent = round(((Khan_Votes / Total_Votes) * 100), 2)
 Correy_Percent = round(((Correy_Votes / Total_Votes) * 100), 2)
-Li_Percent = round(((Li_votes / Total_votes) * 100), 2)
+Li_Percent = round(((Li_Votes / Total_Votes) * 100), 2)
 OTooley_Percent = round(((OTooley_Votes / Total_Votes) * 100), 2)
 print(Khan_Percent)
 print(Correy_Percent)
 print(Li_Percent)
 print(OTooley_Percent)
-    
+
 #Calculating The Winner of The Election
 if Khan_Percent > max(Correy_Percent, Li_Percent, OTooley_Percent):
     winner = "Khan"
@@ -69,15 +70,14 @@ else:
     winner = "O'Tooley"
 
 #Print To Get Final Analysis
-print(f"Election Results", file=text_file)
-print(f"-----------------------------------", file=text_file) 
-print(f"Total Votes: {Total_Votes}", file=text_file)
-print(f"-----------------------------------", file=text_file) 
-print(f"Khan: {Khan_Percent}% ({Khan_Votes})", file=text_file)
-print(f"Correy: {Correy_Percent}% ({Correy_Votes})", file=text_file)
-print(f"Li: {Li_Percent}% ({Li_Votes})", file=text_file)
-print(f"OTooley: {OTooley_Percent}% ({OTooley_Votes})", file=text_file)
-print(f"-----------------------------------", file=text_file)
-print(f"winner: {winner}", file=text_file)
-print(f"-----------------------------------")
-text_file.close()
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {Total_Votes}")
+print("-------------------------")
+print(f"Khan: {Khan_Percent}% ({Khan_Votes}")
+print(f"Correy: {Correy_Percent}% ({Correy_Votes}")
+print(f"Li: {Li_Percent}% ({Li_Votes}")
+print(f"OTooley: {OTooley_Percent}% ({OTooley_Votes}")
+print("-------------------------")
+print(f"winner: {winner}")
+print("-------------------------")
